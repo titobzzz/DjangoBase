d779965db9659758df8bb5dd1239cc49b34b53ee
your_username <olumoyinjoshua19609@gmail.com>
2023-07-26 10:06:42 +0100

    USerMOdelerror

--- base/models.py
+++ base/models.py
@@ -1,7 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-# # # Create your models here.
+# # # # Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(max_length=250,null=True)
+    email=models.EmailField(unique=True)
+    bio=models.TextField(null=True)
+    avatar= models.ImageField(null=True, default=)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS=[]
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=200)
@@ -15,7 +25,7 @@ class Room(models.Model):
     topic =  models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null= True, blank= True)
-    # participants =
+    participants = models.ManyToManyField(User, related_name='participant', blank=True)
     updated = models.DateTimeField(auto_now = True)
     created = models.DateTimeField(auto_now_add=True)
 
@@ -27,13 +37,15 @@ class Room(models.Model):
     
 
 
-
 class Message(models.Model):
     user= models.ForeignKey(User, on_delete=models.CASCADE)
     room  = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now = True)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated','-created']
 
     def __str__(self):
         return self.body[0:50]
